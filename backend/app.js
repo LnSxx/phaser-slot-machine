b17b240be4914a44a3b55b0f5972368de8593065
Leonid Semenov <ln.sm.study@gmail.com>
2021-02-05 21:29:45 +0300

    Fix the import of LISTEN_PORT & README file extension.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const LISTEN_PORT = require('./settings')
+const {LISTEN_PORT} = require('./settings')
 
 app.use(cors())
 
